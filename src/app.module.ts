3aa79bd18c3bddb45bd98992ec2a83bb013b3c4e
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-11-18 19:45:13 +0700

    feat: add database pg module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,11 @@ import { EventEmitterModule } from '@nestjs/event-emitter';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { DatabaseModule } from './shared/libs/pg/database.module';
+import { ConfigModule } from './shared/module';
+
 @Module({
-  imports: [EventEmitterModule.forRoot()],
+  imports: [EventEmitterModule.forRoot(), ConfigModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
