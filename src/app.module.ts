71366ae4f1402852529c69702c0f2e2b95560bad
Arif Ramdani <108125940+shelllbyyyyyy@users.noreply.github.com>
2024-11-18 19:49:04 +0700

    feat: add tokenizer module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,13 @@ import { AppService } from './app.service';
 
 import { RedisModule } from './shared/libs/redis/redis.module';
 import { DatabaseModule } from './shared/libs/pg/database.module';
-import { ConfigModule } from './shared/module';
+import { ConfigModule, JwtModule } from './shared/module';
 
 @Module({
   imports: [
     EventEmitterModule.forRoot(),
     ConfigModule,
+    JwtModule,
     RedisModule,
     DatabaseModule,
   ],
